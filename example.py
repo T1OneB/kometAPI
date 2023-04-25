0a5be29739f87aa9c39d2ddfd0dc06b529642b34
T1OneB (Pavel) <108620914+T1OneB@users.noreply.github.com>
2023-04-25 22:39:26 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,15 @@
+# ПРИМЕТ РАБОТЫ БИБЛИОТЕКИ kometAPI
+
+# Импортируем API библиотеку
+import KometAPI as api
+
+# API ключ
+token="YOUR API KEY HERE"
+# Вызываем функцию получения информации об аккаунте по его API ключу
+a = api.get.account(api_key=token)
+# Выводим результат функции в консоль
+print(a)
+
+# {'ok': True, 'data': {'id': СКРЫЛ, 'balance': 'СКРЫЛ', 'bonuses': СКРЫЛ, 'deposits': 'СКРЫЛ', 'discount': СКРЫЛ, 'refer_balance': 'СКРЫЛ', 'refers': СКРЫЛ}, 'timezone': 'Europe/Moscow', 'time': СКРЫЛ}
+
+# Если в начале 'ok': True , это значит, что запрос был успешно отправлен и получен
